9c37ee4182aed57a27aeb1141ce1804a92bdfdda
Michael Naumov <mnaoumov@gmail.com>
2026-04-08 12:27:38 -0600

    chore: update libs

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,4 @@ import type {} from 'obsidian-typings';
 
 import { Plugin } from './Plugin.ts';
 
-// eslint-disable-next-line import-x/no-default-export -- Obsidian infrastructure requires a default export.
 export default Plugin;
